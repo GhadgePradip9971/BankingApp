e046c7a2301186d05e75f3b67cc3acbbe83e634f
Pradip Ghadge <pradipghadge17@gmail.com>
2026-07-19 19:42:01 +0530

    coding for closing account model will be done

--- BankingApp-SDFC/src/main/java/com/bank/dto/CloseAccountDTO.java
+++ BankingApp-SDFC/src/main/java/com/bank/dto/CloseAccountDTO.java
@@ -1,10 +1,15 @@
 package com.bank.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class CloseAccountDTO {
 
+	@NotBlank(message="Account number not be null")
 	private String accountNumber;
-	private String reasonForClosure;
+	@NotBlank(message="aadhar Number not be null")
+	@Pattern(regexp="^\\d{12}$", message="aadhar number must be 12 digits")
+	private String aadharNumber;
 }
